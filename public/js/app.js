1a4170dd5f46cd92415cc65d98609710119dcd56
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-09 09:26:57 +0200

    up

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,20 @@ console.log('hello ts');
 function sayHi() {
     console.log('Hello from sayHi fn');
 }
+let countVal = 0;
 const btn1El = document.getElementById('btn1');
+const btn2El = document.getElementById('btn2');
+const countePEl = document.getElementById('counter-val');
 console.log('btn1El ===', btn1El);
 btn1El === null || btn1El === void 0 ? void 0 : btn1El.addEventListener('click', (event) => {
     console.log('event.target ===', event.target);
     sayHi();
 });
+btn2El === null || btn2El === void 0 ? void 0 : btn2El.addEventListener('click', goUp);
+function goUp() {
+    countVal++;
+    if (countePEl === null)
+        return;
+    countePEl.textContent = countVal.toString();
+}
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,12 @@ function sayHi(): void {
 }
 // sayHi();
 
+let countVal: number = 0;
+
 // taikomes
 const btn1El = document.getElementById('btn1') as HTMLButtonElement | null;
+const btn2El = document.getElementById('btn2') as HTMLButtonElement | null;
+const countePEl = document.getElementById('counter-val') as HTMLParagraphElement | null;
 
 console.log('btn1El ===', btn1El);
 
@@ -20,3 +24,10 @@ btn1El?.addEventListener('click', (event: Event): void => {
 
 // nusitaikyti i antra mygtuka ir i p su value
 // paspaudus mygtuka noresim padidinti value ir irasyti i html
+btn2El?.addEventListener('click', goUp);
+
+function goUp(): void {
+  countVal++;
+  if (countePEl === null) return;
+  countePEl.textContent = countVal.toString();
+}
